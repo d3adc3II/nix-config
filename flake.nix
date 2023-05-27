91b2369dbf204e03e01a01d28479d1cae2f37751
d3adc3II <ph0ngdinh@proton.com>
2023-05-27 19:06:19 +0800

    first commit

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "D3's NixOS Flake";
+
+  # inputs` are the dependencies of the flake.
+  # `outputs` function will return all the build results of the flake. 
+  # Each item in `inputs` will be passed as a parameter to the `outputs` function after being pulled and built.
+  inputs = {
+
+    # Official NixOS package source, using nixos-unstable branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    # Home Manager Input
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # helix editor, use tag 23.05
+    helix.url = "github:helix-editor/helix/23.05";
+
+  };
+
+  # `outputs` are all the build result of the flake. 
+  # A flake can have many use cases and different types of outputs.
+  # parameters in `outputs` are defined in `inputs` and can be referenced by their names. 
+  # However, `self` is an exception, This special parameter points to the `outputs` itself (self-reference)
+  # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
+  #outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {  
+    nixosConfigurations = {
+      # By default, NixOS will try to refer the nixosConfiguration with its hostname.
+      # so the system named `nixos-test` will use this configuration.
+      # However, the configuration name can also be specified using `sudo nixos-rebuild switch --flake /path/to/flakes/directory#<name>`.
+      # The `nixpkgs.lib.nixosSystem` function is used to build this configuration, the following attribute set is its parameter.
+      # Run `sudo nixos-rebuild switch --flake .#nixos-test` in the flake's directory to deploy this configuration on any NixOS system
+      # Test Config machine
+      nixos-test = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = inputs;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+      # Main nixos config
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = inputs;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.d3 = import ./home.nix;
+            #home-manager.extraSpecialArgs = inputs;          
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,79 @@
+{ config, pkgs, ... }:
+
+{
+  # please change the username & home direcotry to your own
+  home.username = "d3";
+  home.homeDirectory = "/home/d3";
+
+  # link the configuration file in current directory to the specified location in Home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "d3adc3II";
+    userEmail = "ph0ngdinh@proton.com";
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = [ 
+    pkgs.htop
+    pkgs.btop
+    pkgs.kitty
+    pkgs.brave
+  ];
+
+  # enable starship, a beautiful shell prompt
+  #programs.starship = {
+  #  enable = true;
+  #  settings = {
+  #    add_newline = false;
+  #    aws.disabled = true;
+  #    gcloud.disabled = true;
+  #    line_break.disabled = true;
+  #  };
+  #};
+
+  # alacritty terminal emulator
+  #programs.alacritty = {
+  #  enable = true;
+      #env.TERM = "xterm-256color";
+  #    font = {
+  #      size = 12;
+  #      draw_bold_text_with_bright_colors = true;
+  #    };
+  #    scrolling.multiplier = 5;
+  #    selection.save_to_clipboard = true;
+  #};
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
