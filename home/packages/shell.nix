54a7b3d1b26cc7dfd29f200ff4ccaed7d2948b81
d3adc3II <ph0ngdinh@proton.com>
2023-05-28 15:43:56 +0800

    update zsh

--- home/packages/shell.nix
+++ home/packages/shell.nix
@@ -20,10 +20,10 @@
 
   programs.zsh = {
     enable = true;
-    plugins = [
-      { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-      { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-    ];
+   # plugins = [
+   #   { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+   #   { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+   # ];
   # Alias for zsh  
     shellAliases = {
     ll = "ls -l";
