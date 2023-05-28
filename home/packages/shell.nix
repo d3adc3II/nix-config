5c9b40e9aaac0377e50300669d5c5b2656fed8bb
d3adc3II <ph0ngdinh@proton.com>
2023-05-28 17:31:27 +0800

    add zsh, ohmyzsh config

--- home/packages/shell.nix
+++ home/packages/shell.nix
@@ -14,23 +14,32 @@
     discord
     zsh-powerlevel10k
     zplug
-
-    
+    oh-my-zsh
   ];
+  #programs.zsh.zplug.enable =  true;
+  #:wprograms.zsh.zsh-powerlevel10k.enable =  true;
 
   programs.zsh = {
     enable = true;
-    plugins = [
+    enableCompletion = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "superjarin";
+      plugins = [
+        "direnv"
+      ];
+    };
+    #plugins = [
     #  { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
     #  { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-    ];
+    #];
   # Alias for zsh  
     shellAliases = {
     ll = "ls -l";
     update = "sudo nixos-rebuild switch";
     gac = "git add . && git commit -m";
     gp = "git push origin";
-
+  #home.file.".config/oh-my-zsh/themes/af-no-magic.zsh-theme".source = ./af-no-magic.zsh-theme;
   };
   history = {
     size = 10000;
