dfa8c21ff4fa2a1de8bccce233128312af3c3230
d3adc3II <ph0ngdinh@proton.com>
2023-05-28 15:30:22 +0800

    v0.0.2

--- home/packages/shell.nix
+++ home/packages/shell.nix
@@ -1,7 +1,7 @@
 { config, pkgs, helix, ... }:
 {
 
-  # Packages that should be installed to the user profile.
+  # Packages that should be installed to the user profile. Test.
   home.packages = with pkgs; [ 
     kitty
     alacritty
