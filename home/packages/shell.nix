a4d9bf133e8a88cf3653d4cce9c34b90dd191e62
d3adc3II <ph0ngdinh@proton.com>
2023-05-28 15:38:13 +0800

    v0.0.2

--- home/packages/shell.nix
+++ home/packages/shell.nix
@@ -18,6 +18,11 @@
 
   programs.zsh = {
     enable = true;
+    plugins = [
+      { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+      { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+    ];
+  # Alias for zsh  
     shellAliases = {
     ll = "ls -l";
     update = "sudo nixos-rebuild switch";
