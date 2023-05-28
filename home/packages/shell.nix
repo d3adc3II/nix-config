6b6f6a44941c00fd4e7d83a9bd48ddccafa27351
d3adc3II <ph0ngdinh@proton.com>
2023-05-28 21:01:29 +0800

    added zsh plugin

--- home/packages/shell.nix
+++ home/packages/shell.nix
@@ -43,8 +43,8 @@
     ll = "ls -l";
     update = "sudo nixos-rebuild switch";
     gac = "git add . && git commit -m";
-    gp = "git push origin v0.0.2";
-    tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+    gp = "git push origin";
+    tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'";
 
   #home.file.".config/oh-my-zsh/themes/af-no-magic.zsh-theme".source = ./af-no-magic.zsh-theme;
   };
