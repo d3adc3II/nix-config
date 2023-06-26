fa731eee9d71817d211da4f913f4ca3734ee7355
d3adc3II <ph0ngdinh@proton.com>
2023-06-26 18:59:12 +0800

    0.0.5

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -1,7 +1,7 @@
 { inputs, lib, config, pkgs, ... }:
 with lib;
-let
-    cfg = config.modules.eww;
+#let
+#    cfg = config.modules.eww;
 
 in {
     options.modules.firefox = { enable = mkEnableOption "firefox"; };
