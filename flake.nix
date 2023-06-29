cf11d6b6500c4acc8f084c130d9c7055049e9079
d3adc3II <ph0ngdinh@proton.com>
2023-06-29 23:35:51 +0800

    0.0.6

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
   # However, `self` is an exception, this special parameter points to the `outputs` itself (self-reference)
   # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
   # Main nixos config
-  outputs = inputs@{ self, nixpkgs, home-manager, nixos-generators, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
     let 
       x64_system = "x86_64-linux";
       x64_specialArgs = {
