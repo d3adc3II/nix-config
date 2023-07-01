f271748b46f4c5b20ba83fe76996040154cc9a2b
d3adc3II <ph0ngdinh@proton.me>
2023-07-01 19:41:33 +0800

    0.0.6

--- modules/hyprland/wayland-apps.nix
+++ modules/hyprland/wayland-apps.nix
@@ -13,7 +13,7 @@
    #   commandLineArgs = [
         # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
         # (only supported by chromium/chrome at this time, not electron) 
-        "--gtk-version=4"
+    #    "--gtk-version=4"
         # make it use text-input-v1, which works for kwin 5.27 and weston
         # "--enable-wayland-ime"
 
